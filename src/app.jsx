72725d52d39f280fb4a9ff98df3f43b2afd0fd69
Andres Felipe Contreras Mendez <contrerandres001@gmail.com>
2025-07-08 10:21:34 -0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,7 @@
-import LoginForm from "./components/LoginForm";
-
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <LoginForm />
+    <div className="bg-red-600 text-white text-4xl p-10 rounded-xl">
+      ¡Tailwind + React FUNCIONA! 🚀
     </div>
   );
 }
